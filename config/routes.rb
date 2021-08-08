aa2f5758a5e7f7505830c1d331ddc890946e85e3
Misako Kondoh <kondoh.misako@gmail.com>
2021-08-08 18:34:07 +0900

    Changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top', to: 'home#top'
+  get '/', to: 'home#top'
   get 'about', to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
