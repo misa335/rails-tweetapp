1a72185c3109484c51074fd8c99620433fb515c5
Misako Kondoh <kondoh.misako@gmail.com>
2021-08-11 15:23:23 +0900

    add new post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "posts/index", to: "posts#index"
+  get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
   get "/", to: "home#top"
   get "about", to: "home#about"
