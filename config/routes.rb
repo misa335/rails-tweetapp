5306aa60709d9ca0461c081dbc592017a954e0ae
Misako Kondoh <kondoh.misako@gmail.com>
2021-08-11 20:05:11 +0900

    add edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
   post "posts/create", to: "posts#create"
+  get "posts/:id/edit", to: "posts#edit"
+  post "posts/:id/update", to: "posts#update"
 
   get "/", to: "home#top"
   get "about", to: "home#about"
