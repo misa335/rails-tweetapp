aa9481a4ba9b4ad34abee0ff1bac0f24c5244e96
Misako Kondoh <kondoh.misako@gmail.com>
2021-08-11 10:40:49 +0900

    Add posts index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "今日からProgateでRailsの勉強してますー！",
-      "投稿一覧ページ作成中！"
-    ]
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index', to: "posts#index"
-  get '/', to: 'home#top'
-  get 'about', to: 'home#about'
+  get "posts/index", to: "posts#index"
+  get "posts/:id", to: "posts#show"
+  get "/", to: "home#top"
+  get "about", to: "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
