94b7e6f91536027a354443ada1688c32e7b4fe19
Misako Kondoh <kondoh.misako@gmail.com>
2021-08-19 15:37:25 +0900

    Add delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post "posts/create", to: "posts#create"
   get "posts/:id/edit", to: "posts#edit"
   post "posts/:id/update", to: "posts#update"
+  post "posts/:id/destroy", to: "posts#destroy"
 
   get "/", to: "home#top"
   get "about", to: "home#about"
