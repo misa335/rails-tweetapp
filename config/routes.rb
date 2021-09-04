3ba32685c9c82fb4dcf4b7546d342018dab366b5
Misako Kondoh <kondoh.misako@gmail.com>
2021-09-04 21:39:10 +0900

    Add create user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  post "users/create", to: "users#create"
   get "signup", to: "users#new"
-  
   get "users/index", to: "users#index"
   get "users/:id", to: "users#show"
 
